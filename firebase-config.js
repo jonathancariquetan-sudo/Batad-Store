3ff179083fb17ce7e18632fa5d6a76635b37dc31
jonathancariquetan-sudo <jonathancariquetan@gmail.com>
2026-09-13 09:13:42 +0300

    Create firebase-config.js
    window.FIREBASE_CONFIG = {
      apiKey: "AIzaSyCctV7EU6ksi0TNE8HAL6-GteUTLhfI-Wc",
      authDomain: "edgar-store.firebaseapp.com",
      projectId: "edgar-store",
      storageBucket: "edgar-store.firebasestorage.app",
      messagingSenderId: "921087830561",
      appId: "1:921087830561:web:597b6d83eb2b5d35479160"
    };

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCctV7EU6ksi0TNE8HAL6-GteUTLhfI-Wc",
+  authDomain: "edgar-store.firebaseapp.com",
+  projectId: "edgar-store",
+  storageBucket: "edgar-store.firebasestorage.app",
+  messagingSenderId: "921087830561",
+  appId: "1:921087830561:web:597b6d83eb2b5d35479160"
+};
